3f9e400d92a6d7d8bab43188b98093aa789e124e
Camilo <user@CAmilos-MacBook-Pro.local>
2024-09-09 11:06:12 -0700

    adding meme dog to not found page

--- src/components/Not-found/NotFound.jsx
+++ src/components/Not-found/NotFound.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './NotFound.scss';
+import memeDog from '../../Assets/Images/memedog4.png';
 
 
 function NotFound() {
   return (<>
 
   <div className='not-found-container'>
-  <div className='not-found'>Page Not Found 404 😵</div>
+  <h1 className='not-found-title'>404</h1>
+  <div className='not-found'>We couldn't find that page 😵</div>
+  <img className='meme-dog' src={memeDog} alt='meme-dog'></img>
+
 
   </div>
     </> )
