6f2a1b1858042cca3b82712765443623a996d3ad
Camilo <user@CAmilos-MacBook-Pro.local>
2024-09-10 12:06:18 -0700

    profile done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,83 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import './Profile.scss';
 
+
+const URL = 'http://localhost:8080/profile';
 function Profile() {
-  const {userId}= useParams();
-  return (
-    <div>Profile</div>
-  )
+  const userId = parseInt(localStorage.getItem('userId'), 10);
+  const [profile, setProfile]=useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  const fetchProfile = async ()=>{
+    try{
+      const response = await axios.get(`${URL}/${userId}`)
+      setProfile(response.data)
+    } catch(error){
+      console.error('error fetching profile', error)
+    }finally {
+      setLoading(false);
+    }
+           }; 
+
+
+useEffect(() => {
+    if (userId) {
+        fetchProfile(userId);
+    }
+    
+}, [userId]);
+
+if (loading) {
+    return <div>Loading...</div>;
+}
+
+if (!profile) {
+    return <div>No profile data</div>;
+}
+
+  return (<>
+
+    < div key={profile.id} className='profile-box'>
+       <div className='profile-inner-box'>
+       <img alt='profile_img'
+            className='profile-image' 
+            src={`${URL}${profile.profile_image}`}
+             />
+              <div  className='profile-name'>Hi {profile.name},  Welcome back !</div>
+              
+       </div>
+          
+  
+          <div className='profile-description-box'>
+                      
+                 <div className='profile-mini-container'>
+                   <div className='profile-bold'>About </div>
+                   <p className='profile-description'>{profile.about}</p>
+                 </div>
+  
+                 <div className='profile-mini-container'>
+                   <div className='profile-bold'> Riding Level</div>
+                    <p className='profile-description'>{profile.riding_level}</p>  
+                 </div>
+  
+                 <div className='profile-mini-container'>          
+                     <div className='profile-bold'>Contact</div>
+                     <p className='profile-description'>{profile.email}</p>
+                  </div>                     
+                        
+          </div>
+      
+         
+   </div>
+  
+  
+      
+      
+      </> )
 }
 
 export default Profile
